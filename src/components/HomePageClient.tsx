e7c645cae784e7772a8ba83e96a4d16fa6033edd
google veo3 <veog516@gmail.com>
2025-08-20 16:52:42 +0000

    tools thikvabe show hoy   na

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -112,7 +112,7 @@ export default function HomePageClient({ tools }: HomePageClientProps) {
         </div>
     </section>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredTools.map((tool) => (
           <ToolCard 
             key={tool.id} 
