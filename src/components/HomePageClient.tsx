3e441a50814b81a3085647c10262fe1e7de2f13b
google veo3 <veog516@gmail.com>
2025-08-21 16:43:22 +0000

    header er nicher padding kom daw ar akta divider daw tools and header er

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -46,7 +46,9 @@ export default function HomePageClient({ tools }: HomePageClientProps) {
     <div className="container mx-auto px-4">
       <Header />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-8">
+      <SectionDivider />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredTools.map((tool) => (
           <ToolCard 
               key={tool.id} 
